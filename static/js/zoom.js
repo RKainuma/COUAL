d9d3c7d08eec42e88366111c988ef01908936850
shibuya-r <shibuyaryusuke@gmail.com>
2019-12-11 23:59:32 +0900

    sticky sidebar.

--- static/js/zoom.js
+++ static/js/zoom.js
@@ -1,15 +1,7 @@
 $(document).ready(function () {
-    $("#original").mousewheel(function (eo, delta, deltaX, deltaY) {
-        e.preventDefault();
-        var imageWidth = $("#original").width();
-        var imageHeight = $("#original").height();
-
-        if (deltaY < 0) {
-            $("#original").width(imageWidth + 50);
-            $("#original").css('height', imageHeight + (50 * imageHeight / imageWidth));
-        } else {
-            $("#original").width(imageWidth - 50);
-            $("#original").css('height', imageHeight + (-50 * imageHeight / imageWidth));
-        }
+    $(window).resize(function () {
+        $("html").height($("#main").height() + $("#original").height());
+        $("body").height($("#main").height() + $("#original").height());
+        $("#sidebar_div").height($("#main").height() + $("#original").height());
     });
 });
