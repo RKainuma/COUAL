55707ea607853f681100f5307b535dffa6b592a1
jackjack.japan1 <kr49kr21@gmail.com>
2020-01-25 14:49:12 +0900

    ディレクトリ構成変更後の不要ファイル削除

--- static/js/zoom.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $(window).resize(function () {
-        $("html").height($("#main").height() + $("#original").height());
-        $("body").height($("#main").height() + $("#original").height());
-        $("#sidebar_div").height($("#main").height() + $("#original").height());
-    });
-});
